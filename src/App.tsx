0f3e23963d2b99460773ed618647fea9db46e37a
ricaramx77 <ricaramx77@outlook.com>
2025-05-26 18:02:40 -0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Fibonacci from './Fibonacci'
+
+function App() {
+
+  return (
+    <>     
+      <h1>Fibonacci</h1>
+      <Fibonacci />
+    </>
+  )
+}
+
+export default App
